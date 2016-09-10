98bc41921fc2d7243bf68957c41a26b7096965dd
edt85 <eric.traister@gmail.com>
2016-09-10 12:18:03 -0400

    comment merge conflict test

--- project1.m
+++ project1.m
@@ -6,7 +6,8 @@ function project1()
 
     %find the number of images in the set
     imageSetSize = size(imageset);
-
+    % comment merge test
+    
     %iterate across all found images
     %for imageIndex = 1:imageSetSize(4)
     for imageIndex = 1:1 
