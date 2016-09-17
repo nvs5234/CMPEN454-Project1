9f58a4f5fdf06efa3bd231a5e6d6c5fc392ffe66
edt85 <eric.traister@gmail.com>
2016-09-17 17:04:44 -0400

    Full connected working

--- layers/fullconnect.m
+++ layers/fullconnect.m
@@ -1,5 +1,9 @@
 function output = fullconnect(in, filterBank, biasVect)
 
+% convert image and filter to doubles
+in = double(in);
+filterBank = double(filterBank);
+
 % find d1
 inSize = size(in);
 d1 = inSize(3);
@@ -8,24 +12,18 @@ d1 = inSize(3);
 filterBankSize = size(filterBank);
 d2 = filterBankSize(4);
 
-% convert image and filter to doubles
-im = double(in);
-filt = double(filterBank);
-
-%initialze sum
-sum = 0;
-
 % apply filter of same size to each dimension of image
 for i = 1:d2
+        sum = 0;
     for j = 1:inSize(1)
         for k = 1:inSize(2)
             for l = 1:d1
-                piece = im(j, k, l);
-                filterl = filt(j, k, l, i);
-                sum = sum + piece * filterl;
+                piece = in(j, k, l);
+                filterl = filterBank(j, k, l, i);
+                sum = sum + imfilter(piece,filterl,'conv','same');
             end
         end
     end
+    output(1,1,i) = sum + biasVect(i);
 end
-output(1,1,i) = sum + biasVect(i);
 end
