85d8f4f2e5116705c823e2d04a3c4865c7195e7d
Nathan Scribano <nscribano@Nathans-MacBook-Pro.local>
2016-09-17 09:19:28 -0400

    Fixed debugging equality check. Works now

--- debugging.m
+++ debugging.m
@@ -1,8 +1,8 @@
-% debugging check for CNN layers
+% Debugging check for CNN layers
 function debugging(filteredImage, stepIndex)
     load debuggingTest.mat
 
-    if filteredImage == layerResults{stepIndex}
+    if isequal(filteredImage, layerResults{stepIndex})
         fprintf('Step %i passes\n',stepIndex)
     else
         fprintf('Step %i fails\n',stepIndex)
--- project1.m
+++ project1.m
@@ -1,14 +1,17 @@
 function project1()
-    %loading this file defines imageset, trueclass, and classlabels
+    % Loading this file defines imageset, trueclass, and classlabels
     load cifar10testdata.mat
     load CNNparameters.mat
-    load debuggingTest.mat          % need imrgb for testing
+    load debuggingTest.mat          % imrgb needed for testing
     addpath layers
 
-    %find the number of images in the set
+    % Clear command window for each run
+    clc
+    
+    % Find the number of images in the set
     imageSetSize = size(imageset);
     
-    %iterate across all found images
+    % Iterate across all found images
     %for imageIndex = 1:imageSetSize(4)
     for imageIndex = 1:1 
         %im = double(imageset(:,:,:,imageIndex));
@@ -21,9 +24,13 @@ function project1()
         debugging(im,2);
         
         im = relu(im);
+        debugging(im,3);
+        
+        im = convolve(im,filterbanks{4}, biasvectors{4});
+        debugging(im,4);
     end
 
-    %some sample code to read and display one image from each class
+    % Some sample code to read and display one image from each class
     % for classindex = 1:10
     %     %get indices of all images of that class
     %     inds = find(trueclass==classindex);
