8ecece3ca2bb66e50378b5655f05ee73e8490cdc
edt85 <eric.traister@gmail.com>
2016-09-17 16:33:26 -0400

    Initial fully connected implementation

--- layers/fullconnect.m
+++ layers/fullconnect.m
@@ -1,3 +1,25 @@
-function output = fullconnect(input)
+function output = fullconnect(in, filterBank, biasVect)
 
+% find d1
+inSize = size(in);
+d1 = inSize(3);
+
+% find d2
+filterBankSize = size(filterBank);
+d2 = filterBankSize(4);
+
+% apply filter of same size to each dimension of image
+for i = 1:d2
+    filteri = filterBank(:,:,:,i);
+    for j = 1:inSize(1)
+        for k = 1:inSize(2)
+            for l = 1:d1
+                piece = img(j, k, l);
+                filterl = filter(j, k, l, filteri);
+                sum = sum + piece * filterl;
+            end
+        end
+    end
+end
+                
 end
