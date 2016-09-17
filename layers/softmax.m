5cb89c003205c945ebc9e9e1853fe809bb71eb46
Andrew Lee <quabeeb@gmail.com>
2016-09-17 18:52:11 -0400

    Changes to softmax
    one for loop by using 'sum'

--- layers/softmax.m
+++ layers/softmax.m
@@ -6,18 +6,11 @@ output = in;    % output will be same as input but exponentiated and normalized
 alpha = max(in(1,1,:)); % find alpha across all inputs
 inSize = size(in);
 dimension = inSize(3);
-sumofd = 0;
 
 for i = 1:dimension
-    % compute the sum of the denominator first
-    denominator = exp((in(1,1,i) - alpha));
-    sumofd = sumofd + denominator;
-end
-
-for k = 1:dimension
-        % compute output by normalizing the probabilities
-        numerator = exp((in(1,1,k) - alpha));
-        output(1:1:k) = numerator / sumofd;
-end        
+    numer = exp(in(1,1,i)-alpha);
+    denom = sum(exp(in(1,1,:)-alpha));
+    output(1,1,i) = numer/denom;
+end     
   
 end
